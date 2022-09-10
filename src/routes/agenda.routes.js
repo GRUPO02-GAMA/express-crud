bfec0d41f7dfb5951c1520ec70581cf8b295beb4
Janice Caldeira <janicecaldeira@gmail.com>
2022-09-10 09:38:09 -0300

    feat: create routes

---
+++ src/routes/agenda.routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const agendaController =   require('../controllers/agenda.controller');
+
+router.get('/', agendaController.findAll);
+router.post('/', agendaController.create);
+router.get('/:id', agendaController.findById);
+router.put('/:id', agendaController.update);
+router.delete('/:id', agendaController.delete);
+
+module.exports = router
