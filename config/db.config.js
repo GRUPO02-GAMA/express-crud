4b9bc4f90cc19c739fa61ffaab4d5b36eb7ac6b1
Victor <vmtame@gmail.com>
2022-09-10 09:47:03 -0300

    + env.example

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'nodejs'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE
 });
+
 dbConn.connect(function(err) {
   if (err) throw err;
   console.log("Database Connected!");
