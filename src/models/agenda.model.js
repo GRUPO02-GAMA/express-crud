19fa5e566a8c0b2ba1023673ca0527dc193d87cd
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-09-10 10:07:48 -0300

    feat: create Agenda template class

---
+++ src/models/agenda.model.js
@@ -0,0 +1,8 @@
+class Agenda {
+    constructor(id, name, email, age, gender) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+}
