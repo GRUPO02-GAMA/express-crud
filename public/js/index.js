378635bf982d03784c97e6bb4cea6ca0b881f388
Victor <vmtame@gmail.com>
2022-09-10 11:34:55 -0300

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
-console.log('hello world!');
+fetch('/api/agenda')
+    .then((response) => response.json())
+    .then((res) => {
+        for(let i = 0; i < res.length ; i++) {
+            const el = res[i];
+
+            $('#agenda tbody').append(
+            `
+            <tr>
+                <td>${el.id}</td>
+                <td>${el.name}</td>
+                <td>${el.email}</td>
+                <td>${el.age}</td>
+                <td>${el.gender}</td>
+            </tr>
+                `
+            )
+        }
+    })
+    .catch((err) => {
+        alert(err);
+    });
